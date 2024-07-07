764568760fdb6bf4839f7dcdac452cbdca340d8d
udhos <udhos>
2024-07-06 22:43:10 -0300

    Refresh token.

--- eksclient/client.go
+++ eksclient/client.go
@@ -5,6 +5,8 @@ import (
 	"encoding/base64"
 	"fmt"
 	"log"
+	"net/http"
+	"time"
 
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
@@ -32,6 +34,49 @@ type Options struct {
 
 	// Logf optionally defines logging fuction. If unspecified defaults to log.Printf.
 	Logf func(format string, v ...any)
+
+	// Source optionally defines a custom token source.
+	Source TokenSource
+}
+
+// TokenSource generates tokens.
+type TokenSource interface {
+	Get() (token.Token, error)
+}
+
+type tokenGenerator struct {
+	generator      token.Generator
+	options        *token.GetTokenOptions
+	last           token.Token
+	refreshEarlier time.Duration
+}
+
+func newTokenGenerator(options Options) (*tokenGenerator, error) {
+	gen, err := token.NewGenerator(true, false)
+	if err != nil {
+		return nil, err
+	}
+	opts := &token.GetTokenOptions{
+		ClusterID: options.ClusterName,
+	}
+	return &tokenGenerator{
+		generator:      gen,
+		options:        opts,
+		refreshEarlier: 10 * time.Second,
+	}, nil
+}
+
+func (g *tokenGenerator) Get() (token.Token, error) {
+	if needsRefresh(g.refreshEarlier, g.last.Expiration) {
+		tok, err := g.generator.GetWithOptions(g.options)
+		g.last = tok
+		return tok, err
+	}
+	return g.last, nil
+}
+
+func needsRefresh(refreshEarlier time.Duration, expiration time.Time) bool {
+	return time.Now().Add(refreshEarlier).After(expiration)
 }
 
 // New creates kubernetes client.
@@ -43,33 +88,38 @@ func New(options Options) (*kubernetes.Clientset, error) {
 		options.Logf = log.Printf
 	}
 
+	if options.Source == nil {
+		source, err := newTokenGenerator(options)
+		if err != nil {
+			return nil, err
+		}
+		options.Source = source
+	}
+
 	debugf := func(format string, v ...any) {
 		if options.DebugLog {
 			options.Logf(fmt.Sprintf("DEBUG: %s: ", me)+format, v...)
 		}
 	}
 
-	return newClientset(debugf, options.ClusterName, options.ClusterCAData, options.ClusterEndpoint)
+	return newClientset(debugf, options.Source, options.ClusterName, options.ClusterCAData, options.ClusterEndpoint)
 }
 
 // newClientset creates kubernetes client.
 // FIXME WRITEME TODO XXX Refresh/renew token automatically.
-func newClientset(debugf func(format string, v ...any), clusterName, clusterCAData, clusterEndpoint string) (*kubernetes.Clientset, error) {
+func newClientset(debugf func(format string, v ...any), source TokenSource,
+	clusterName, clusterCAData, clusterEndpoint string) (*kubernetes.Clientset, error) {
 
 	debugf("newClientset: clusterName=%s endpoint=%s CA=%s",
 		clusterName, clusterEndpoint, clusterCAData)
 
-	gen, err := token.NewGenerator(true, false)
-	if err != nil {
-		return nil, err
-	}
-	opts := &token.GetTokenOptions{
-		ClusterID: clusterName,
-	}
-	tok, err := gen.GetWithOptions(opts)
-	if err != nil {
-		return nil, err
-	}
+	/*
+		tok, err := source.Get()
+		if err != nil {
+			return nil, err
+		}
+	*/
+
 	ca, err := base64.StdEncoding.DecodeString(clusterCAData)
 	if err != nil {
 		return nil, err
@@ -78,13 +128,17 @@ func newClientset(debugf func(format string, v ...any), clusterName, clusterCADa
 		&rest.Config{
 			Host: clusterEndpoint,
 
-			// FIXME WRITEME TODO XXX
-			//
-			// Refresh/renew token automatically.
-			BearerToken: tok.Token,
+			//BearerToken: tok.Token,
 
 			TLSClientConfig: rest.TLSClientConfig{
 				CAData: ca,
+			},
+
+			WrapTransport: func(rt http.RoundTripper) http.RoundTripper {
+				return &tokenTransport{
+					source:    source,
+					transport: rt,
+				}
 			},
 		},
 	)
@@ -92,4 +146,20 @@ func newClientset(debugf func(format string, v ...any), clusterName, clusterCADa
 		return nil, err
 	}
 	return clientset, nil
+}
+
+type tokenTransport struct {
+	source    TokenSource
+	transport http.RoundTripper
+}
+
+func (t *tokenTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	tok, err := t.source.Get()
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", tok.Token))
+
+	return t.transport.RoundTrip(req)
 }
